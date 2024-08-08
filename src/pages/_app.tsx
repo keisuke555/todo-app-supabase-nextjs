73ead326724fc2410eeac56a1d4be3f4d383d09c
keisuke555 <ketsun0.po1ice.bz@gmail.com>
2024-08-08 13:43:49 +0900

    initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,29 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps, NextWebVitalsMetric } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
+export function reportWebVitals(metric: NextWebVitalsMetric) {
+  switch (metric.name) {
+    case 'FCP':
+      console.log(`FCP: ${Math.round(metric.value * 10) / 10}`)
+      break
+    case 'LCP':
+      console.log(`LCP: ${Math.round(metric.value * 10) / 10}`)
+      break
+    case 'TTFB':
+      console.log(`TTFB: ${Math.round(metric.value * 10) / 10}`)
+      break
+    case 'Next.js-hydration':
+      console.log(
+        `Hydration: ${Math.round(metric.startTime * 10) / 10} -> ${Math.round((metric.startTime + metric.value) * 10) / 10}`,
+      )
+      break
+    default:
+      break
+  }
+}
+
+function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
+
+export default App
